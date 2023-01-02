ac8cb36cd81cd98f510d7223dd163ff837596ad7
Gauravdhale09 <103017831+Gauravdhale09@users.noreply.github.com>
2023-01-02 19:41:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,60 @@
+import datetime
 import pyaudio
 import pyttsx3
 import vosk
-
-
 from vosk import Model, KaldiRecognizer
+from time import strftime
+import datetime as dt
 engine = pyttsx3.init()
-model = Model(r"K:\Projects\Programming\Python\Extra\Library\Speech Recognition\Vosk\vosk-model-small-en-in-0.4")
-
+model = Model(r"C:\Users\gaura\PycharmProjects\robot\vosk-model-small-en-in-0.4")
 recognizer = KaldiRecognizer(model, 16000)
-
-dicte = {
-"what is your name":"my name is Lucifer",
-"who are you":"I am robo v1.o"
-
-}
-
 mic = pyaudio.PyAudio()
 stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
 stream.start_stream()
-
-
 def listen(i):
     engine.say(i)
     engine.runAndWait()
     engine.setProperty('voice', 'english-us')
     engine.setProperty('rate', 150)
-
 def speak(tex):
-    if tex in dicte:
-        listen(dicte[tex])
     if tex == "okay stop":
         raise SystemExit
-
-
+    elif tex == "showtime":
+        string = strftime('%H:%M')
+        now = datetime.datetime.now().time()
+        if  12>=now.hour>6:
+            listen("Good morning")
+            print(string)
+            listen("its"+string)
+        elif 17>=now.hour>12:
+            listen("Good afternoon")
+            print(string)
+            listen("its"+string)
+        elif 21>=now.hour>17:
+            listen("Good Evening")
+            print(string)
+            listen("its"+string)
+        elif 00>=now.hour>21:
+            listen("Good night")
+            print(string)
+            listen("its"+string)
+        else:
+            listen(string)
+    elif tex == "who am i":
+        listen("gaurav great person")
+    elif tex ==  "who are you":
+        listen("I am robo v1.o")
+    elif tex ==  "my college":
+        listen("shri sant gajanan maharaj college of engineering shegon")
+    elif tex ==  "head of department":
+        listen("doctor s b patil")
+    elif tex ==  "my branch":
+        listen("computer science and engineering")
 
 while True:
-
     data = stream.read(4096,exception_on_overflow=False)
     if recognizer.AcceptWaveform(data):
         text = recognizer.Result()
         text1 = text[14:-3]
         print(text1)
         speak(text1)
-
-
-
-
-
-
-
