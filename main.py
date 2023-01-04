90b4e9e8562442c95fb1bf37751dbf59b7a40f2c
Gauravdhale09 <103017831+Gauravdhale09@users.noreply.github.com>
2023-01-04 20:44:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,28 @@ import pyttsx3
 import vosk
 from vosk import Model, KaldiRecognizer
 from time import strftime
-import datetime as dt
 
-engine = pyttsx3.init()
-model = Model(r"K:\Projects\Programming\Python\Extra\Library\Speech Recognition\Vosk\vosk-model-small-en-in-0.4")
+engine = pyttsx3.init('sapi5')
+model = Model(r"C:\Users\gaura\PycharmProjects\robot\vosk-model-en-in-0.5")
 recognizer = KaldiRecognizer(model, 16000)
 mic = pyaudio.PyAudio()
-stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+stream = mic.open(format=pyaudio.paInt16, channels=1,
+                  rate=16000, input=True, frames_per_buffer=8192)
 stream.start_stream()
 
-
 def listen(i):
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice',voices[2].id)
+    engine.setProperty('rate',130)
     engine.say(i)
     engine.runAndWait()
-    engine.setProperty('voice', 'english-us')
-    engine.setProperty('rate', 150)
+
+
 
 
 def speak(tex):
     if tex == "okay stop":
+        listen("okay")
         raise SystemExit
     elif tex == "showtime":
         string = strftime('%H:%M')
@@ -30,39 +33,48 @@ def speak(tex):
         if 12 >= now.hour > 6:
             listen("Good morning")
             print(string)
-            listen("its" + string)
+            listen("its"+string)
         elif 17 >= now.hour > 12:
             listen("Good afternoon")
             print(string)
-            listen("its" + string)
+            listen("its"+string)
         elif 21 >= now.hour > 17:
             listen("Good Evening")
             print(string)
-            listen("its" + string)
+            listen("its"+string)
         elif 00 >= now.hour > 21:
             listen("Good night")
             print(string)
-            listen("its" + string)
+            listen("its"+string)
         else:
             listen(string)
+    elif tex == "today's date":
+        date = datetime.datetime.now()
+        listen(f'{date:%b %d %Y}')
+    elif tex == "hi":
+        listen("Hello")
     elif tex == "who are you":
-        listen("I am robo v1.o")
+        listen("I am sika the robo")
+    elif tex == "my college":
+        listen("shri sant gajanan maharaj college of engineering ssgmce")
     elif tex == "head of computer science":
-        listen("Doctor s b Patil")
-        elif tex == "head of electrical engineering":
+        listen("doctor s b patil")
+    elif tex == "head of electrical engineering":
         listen("Doctor S R Paraskar")
     elif tex == "head of information technology":
-        listen("Doctor A S Manekar")
-    elif tex == "head of electronics department":
+        listen("Doctor A S Maanekar")
+    elif tex == "head of electronics":
         listen("Doctor M N Tibdewal")
-    elif tex == "head of applied science and humanities department":
+    elif tex == "head of applied science and humanities":
         listen("Doctor N A Patil")
     elif tex == "head of mechanical engineering":
-        listen("Doctor S P Trikal")
-
-    elif tex == "my branch":
-        listen("computer science and engineering")
-
+        listen("Doctor S P Trikaal")
+    elif tex == "thanks":
+        listen("its my pleasure")
+    elif tex == "name of our library":
+        listen("pradnya chakshu gulaabraao granthalayaa")
+    elif tex == "principal of our college":
+        listen("doctor s b somaani")
 
 while True:
     data = stream.read(4096, exception_on_overflow=False)
